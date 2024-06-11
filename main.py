0a51a4c2767f6a2704a5f6ed06387cb433a42634
Kislay Panday <157260183+PK12345Me@users.noreply.github.com>
2024-06-11 17:13:36 +0800

    Update main.py
    added minor fixes in the loop

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import random
-list = ["H", "T"]
-listB = []
+
 i = 0
 
-listA = ["H", "H", "H"]
+# Generate a list of 10,000 random choices between "H" and "T"
+listA = ["H", "H", "H", "H", "H", "H"]
 
 total = 10000
-for _ in range(total):
-    listB += random.choice(list)
+listB = [random.choice(["H", "T"]) for _ in range(total)]
 
+# creates a pattern
 s = "".join(x for x in listB)
-#print(s)
 o = "".join(x for x in listA)
-#print(o)
 
+#actual caculation
 x = 0
 while x < len(s):
-    if o in s[x:x+3]:
-        #print("x2", x)
-        #print("x", x, s[x:x+3])
-        x += 3  # Increase x by 3 to skip the next few elements
+    if o in s[x:x+6]:
+        # Increase x by 6 to skip the next few elements
+        x += 6
+        # Increase i by 1 to record presence of the desired pattern
         i += 1
     else:
-        x += 1  # Only increment x by 1 to move to the next element
+        # Only increment x by 1 to move to the next element
+        x += 1
 
 op = float(i/total)
-print(f"chances of a streak(6*H) in 10k tries are ",op,"%")
+print(f"chances of a streak(6*H) in 10k tries are {op:.6f} %")
